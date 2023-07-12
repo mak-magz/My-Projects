76293c330445280e4416535f5e8533126e4f533e
magcanta <magcanta@mobileit.co.jp>
2023-07-12 17:41:56 +0900

    added note list

--- MyNote/app/src/main/java/com/practice/mynote/ui/screens/my_note_app/MyNoteApp.kt
+++ MyNote/app/src/main/java/com/practice/mynote/ui/screens/my_note_app/MyNoteApp.kt
@@ -2,8 +2,16 @@ package com.practice.mynote.ui.screens.my_note_app
 
 import android.content.res.Configuration.UI_MODE_NIGHT_NO
 import android.content.res.Configuration.UI_MODE_NIGHT_YES
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material.icons.filled.MoreVert
@@ -11,12 +19,18 @@ import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.practice.mynote.ui.theme.MyNoteTheme
 
 @Composable
@@ -46,8 +60,46 @@ fun MyNoteApp() {
 			)
 		}
 	) { contentPadding ->
-		Column(modifier = Modifier.padding(contentPadding)) {
+		val titles = listOf(
+			"First",
+			"second",
+			"third"
+		)
+//		Column(modifier = Modifier.padding(contentPadding)) {
+//
+//		}
+		LazyColumn(
+			modifier = Modifier
+				.padding(contentPadding)
+				.fillMaxWidth()
+		) {
+			items(items = titles) { title ->
+				Note(title = title)
+				Spacer(modifier = Modifier.size(10.dp))
+			}
+		}
+	}
+}
 
+@Composable
+fun Note(title: String, modifier: Modifier = Modifier) {
+	Row(
+		modifier = modifier
+			.fillMaxWidth()
+			.background(color = MaterialTheme.colorScheme.onPrimary)
+			.padding(5.dp),
+		verticalAlignment = Alignment.CenterVertically
+	) {
+		Box(
+			modifier = Modifier
+				.size(50.dp)
+				.background(Color.Red)
+				.clip(RectangleShape)
+		)
+		Spacer(modifier = Modifier.padding(end = 5.dp))
+		Column {
+			Text(text = title)
+			Text(text = "date")
 		}
 	}
 }
